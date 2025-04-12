ef13130f71b5d770d519b5804fb8b28d1bdac03e
Nini <nwen@radioflyer.com>
2025-04-12 11:03:01 +0800

    update code blocks config3

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -93,9 +93,7 @@ export default defineUserConfig({
       // whitespace: true, // 启用 空格/Tab 高亮
       lineNumbers: true, // 启用行号
     },
-    plugins: {
-      shiki: {}
-    },
+    
 
     /* 文章字数统计、阅读时间，设置为 false 则禁用 */
     // readingTime: true,
@@ -110,22 +108,22 @@ export default defineUserConfig({
       pdf: true,          // 启用 PDF 嵌入 @[pdf](/xxx.pdf)
       caniuse: true,      // 启用 caniuse 语法  @[caniuse](feature_name)
       plot: true,         // 启用隐秘文本语法 !!xxxx!!
-      bilibili: true,     // 启用嵌入 bilibili视频 语法 @[bilibili](bid)
-      youtube: true,      // 启用嵌入 youtube视频 语法 @[youtube](video_id)
-      artPlayer: true,    // 启用嵌入 artPlayer 本地视频 语法 @[artPlayer](url)
-      audioReader: true,  // 启用嵌入音频朗读功能 语法 @[audioReader](url)
+      // bilibili: true,     // 启用嵌入 bilibili视频 语法 @[bilibili](bid)
+      // youtube: true,      // 启用嵌入 youtube视频 语法 @[youtube](video_id)
+      // artPlayer: true,    // 启用嵌入 artPlayer 本地视频 语法 @[artPlayer](url)
+      // audioReader: true,  // 启用嵌入音频朗读功能 语法 @[audioReader](url)
       icons: true,        // 启用内置图标语法  :[icon-name]:
-      codepen: true,      // 启用嵌入 codepen 语法 @[codepen](user/slash)
-      replit: true,       // 启用嵌入 replit 语法 @[replit](user/repl-name)
-      codeSandbox: true,  // 启用嵌入 codeSandbox 语法 @[codeSandbox](id)
-      jsfiddle: true,     // 启用嵌入 jsfiddle 语法 @[jsfiddle](user/id)
-      npmTo: true,        // 启用 npm-to 容器  ::: npm-to
-      demo: true,         // 启用 demo 容器  ::: demo
-      repl: {             // 启用 代码演示容器
-        go: true,         // ::: go-repl
-        rust: true,       // ::: rust-repl
-        kotlin: true,     // ::: kotlin-repl
-      },
+      // codepen: true,      // 启用嵌入 codepen 语法 @[codepen](user/slash)
+      // replit: true,       // 启用嵌入 replit 语法 @[replit](user/repl-name)
+      // codeSandbox: true,  // 启用嵌入 codeSandbox 语法 @[codeSandbox](id)
+      // jsfiddle: true,     // 启用嵌入 jsfiddle 语法 @[jsfiddle](user/id)
+      // npmTo: true,        // 启用 npm-to 容器  ::: npm-to
+      // demo: true,         // 启用 demo 容器  ::: demo
+      // repl: {             // 启用 代码演示容器
+      //   go: true,         // ::: go-repl
+      //   rust: true,       // ::: rust-repl
+      //   kotlin: true,     // ::: kotlin-repl
+      // },
       math: {             // 启用数学公式
         type: 'katex',
       },
@@ -140,7 +138,7 @@ export default defineUserConfig({
         size: true,       // 启用图片大小
       },
       // include: true,      // 在 Markdown 文件中导入其他 markdown 文件内容
-      imageSize: 'local', // 启用 自动填充 图片宽高属性，避免页面抖动
+      // imageSize: 'local', // 启用 自动填充 图片宽高属性，避免页面抖动
     },
 
     /**
@@ -170,5 +168,10 @@ export default defineUserConfig({
      * @see https://theme-plume.vuejs.press/guide/features/encryption/
      */
     // encrypt: {},
+
+    plugins: {
+      shiki: {},
+      markdownPower: {}
+    },
   }),
 })
